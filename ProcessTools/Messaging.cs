3226eb04384bdec34e8eaa040302e1906383a7b4
Erik Nagel <erik@reallyhuman.net>
2024-03-23 12:50:22 +0100

    New classfiles ProcessWorker.cs and Messaging.cs added.

---
+++ ProcessTools/Messaging.cs
@@ -0,0 +1,38 @@
+﻿using System.Runtime.InteropServices;
+
+namespace NetEti.ApplicationControl
+{
+    // Special thanks to Matt Davis, https://stackoverflow.com/users/51170/matt-davis
+    // this class just wraps some Win32 stuff that we're going to use
+
+    /// <summary>
+    /// Provides Windows message handling via System.Runtime.InteropServices.
+    /// </summary>
+    public static class Messaging
+    {
+        /// <summary>Broadcast message flag.</summary>
+        public const int HWND_BROADCAST = 0xffff;
+
+        /// <summary>Registered message type for a Window activation request.</summary>
+        public static readonly int WM_SHOWME = RegisterWindowMessage("WM_SHOWME");
+
+        /// <summary>
+        /// Wrapper for external PostMessage.
+        /// </summary>
+        /// <param name="hwnd">Window handle.</param>
+        /// <param name="msg">Message to be posted.</param>
+        /// <param name="wparam">Pointer to the message.</param>
+        /// <param name="lparam">Length of the message.</param>
+        /// <returns>True on success.</returns>
+        [DllImport("user32")]
+        public static extern bool PostMessage(IntPtr hwnd, int msg, IntPtr wparam, IntPtr lparam);
+
+        /// <summary>
+        /// Wrapper for external RegisterMessage.
+        /// </summary>
+        /// <param name="message">The message to listen for.</param>
+        /// <returns>Message-ID on success (0xC000-0xFFFF) or Zero on Error.</returns>
+        [DllImport("user32")]
+        public static extern int RegisterWindowMessage(string message);
+    }
+}
---
+++ ProcessTools/ProcessWorker.cs
@@ -0,0 +1,79 @@
+﻿using System.Diagnostics;
+using System.Management;
+
+namespace NetEti.ApplicationControl
+{
+    /// <summary>
+    /// Statische Routinen für den Umgang mit Prozessen.
+    /// </summary>
+    public static class ProcessWorker
+    {
+        #region public members
+
+        /// <summary>
+        /// Wartet, bis alle Child-Prozesse beendet sind oder ein Countdown abgelaufen ist.
+        /// Wenn bei abgelaufenem Countdown noch nicht alle Prozesse beendet sind, werden sie abgeschossen.
+        /// </summary>
+        /// <param name="process">Der Prozess, dessen Child-Prozesse beendet werden sollen.</param>
+        /// <param name="countdown">Zählt rückwärts, bei 1 werden alle Child-Prozesse beendet (default=6).</param>
+        public static void FinishChildProcesses(Process process, int countdown = 6)
+        {
+            UInt32 pid = (UInt32)(process.Id);
+            Thread t = new Thread(() => WaitForChildProcessesCloseOrKillThemAtLast(pid, countdown));
+            t.Start();
+            while (t.IsAlive)
+            {
+                Thread.Sleep(500);
+            }
+        }
+
+        #endregion public members
+
+        #region private members
+
+        private static void WaitForChildProcessesCloseOrKillThemAtLast(UInt32 pid, int countdown = 6)
+        {
+            HandleAllProcessesSpawnedBy(pid);
+        }
+
+        // https://stackoverflow.com/questions/7189117/find-all-child-processes-of-my-own-net-process-find-out-if-a-given-process-is
+        // https://stackoverflow.com/questions/58216860/how-to-handle-not-enough-quota-is-available-to-process-this-command-exception
+        private static void HandleAllProcessesSpawnedBy(UInt32 parentProcessId, int countdown = 6)
+        {
+            if (!OperatingSystem.IsWindows())
+            {
+                throw new PlatformNotSupportedException("System.Management ist nur für Windows implementiert.");
+            }
+            // NOTE: Process Ids are reused!
+            for (int i = countdown; i > 0; i--)
+            {
+                using (ManagementObjectSearcher searcher = new ManagementObjectSearcher(
+                    "SELECT * " +
+                    "FROM Win32_Process " +
+                    "WHERE ParentProcessId=" + parentProcessId))
+                {
+                    ManagementObjectCollection collection = searcher.Get();
+                    if (collection.Count < 1) { break; } // Nichts zu tun, Routine verlassen.
+                    if (i > 1) { Thread.Sleep(250); continue; } // Warten und nächster Durchlauf.
+                    foreach (var item in collection) // Letzter Durchlauf, Prozess abschießen.
+                    {
+                        UInt32 childProcessId = (UInt32)item["ProcessId"];
+                        if ((int)childProcessId != Process.GetCurrentProcess().Id)
+                        {
+                            HandleAllProcessesSpawnedBy(childProcessId, i);
+                            try
+                            {
+                                Process childProcess = Process.GetProcessById((int)childProcessId);
+                                childProcess.Kill();
+                            }
+                            catch { }
+                            // MessageBox.Show("Child process " + childProcessId + " killed (Countdown: " + i + ").");
+                        }
+                    }
+                }
+            }
+        }
+
+        #endregion private members
+    }
+}
